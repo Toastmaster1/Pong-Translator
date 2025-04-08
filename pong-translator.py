60f8bf32b9c32962f93808e6f51e6354da74ccd5
Toastmaster1 <dylanlfarmer@hotmail.com>
2025-04-08 22:04:48 +0100

    Quick Pong to English
    Credit to AJH for this

--- pong-translator.py
+++ pong-translator.py
@@ -120,131 +120,73 @@ def eng_to_pong():
     translated += ")"
     print(translated)
 
+# Credit to AJH for this subroutine and the next one
+def pong_to_eng_dict():
+    return {
+        "pong": "a",
+        "p ong": "b",
+        "po ng": "c",
+        "pon g": "d",
+        "p o ng": "e",
+        "po n g": "f",
+        "p on g": "g",
+        "p o n g": "h",
+        "Pong": "i",
+        "P ong": "k",
+        "Po ng": "j",
+        "Pon g": "l",
+        "P o ng": "m",
+        "Po n g": "n",
+        "P on g": "o",
+        "P o n g": "p",
+        "pOng": "q",
+        "p Ong": "r",
+        "pO ng": "s",
+        "pOn g": "t",
+        "p O ng": "u",
+        "pO n g": "v",
+        "p On g": "w",
+        "p O n g": "x",
+        "poNg": "y",
+        "p oNg": "z",
+    }
 
-def pong_to_eng():
-    end = False
-    translated = ""
-    print("Enter each pong individually, starting at P and ending at G.")
-    print("If there is a )( please enter a space.")
-    print("Enter 'end' to finish translating.")
-    while end is not True:
-        print("ENTER THE NEXT PONG: ")
-        pong = input()
-        if pong == "pong":
-            translated += "a"
-        elif pong == "ponG":
-            translated += "A"
-        elif pong == "p ong":
-            translated += "b"
-        elif pong == "p onG":
-            translated += "B"
-        elif pong == "po ng":
-            translated += "c"
-        elif pong == "po nG":
-            translated += "C"
-        elif pong == "pon g":
-            translated += "d"
-        elif pong == "pon G":
-            translated += "D"
-        elif pong == "p o ng":
-            translated += "e"
-        elif pong == "p o nG":
-            translated += "E"
-        elif pong == "po n g":
-            translated += "f"
-        elif pong == "F":
-            translated += "po n G"
-        elif pong == "p on g":
-            translated += "g"
-        elif pong == "p on G":
-            translated += "G"
-        elif pong == "p o n g":
-            translated += "h"
-        elif pong == "p o n G":
-            translated += "H"
-        # i to p
-        elif pong == "Pong":
-            translated += "i"
-        elif pong == "PonG":
-            translated += "I"
-        elif pong == "P ong":
-            translated += "j"
-        elif pong == "P onG":
-            translated += "J"
-        elif pong == "Po ng":
-            translated += "k"
-        elif pong == "Po nG":
-            translated += "K"
-        elif pong == "Pon g":
-            translated += "l"
-        elif pong == "Pon G":
-            translated += "L"
-        elif pong == "P o ng":
-            translated += "m"
-        elif pong == "P o nG":
-            translated += "M"
-        elif pong == "Po n g":
-            translated += "n"
-        elif pong == "Po n G":
-            translated += "N"
-        elif pong == "P on g":
-            translated += "o"
-        elif pong == "P on G":
-            translated += "O"
-        elif pong == "P o n g":
-            translated += "p"
-        elif pong == "P o n G":
-            translated += "P"
-        # q to x
-        elif pong == "pOng":
-            translated += "q"
-        elif pong == "pOnG":
-            translated += "Q"
-        elif pong == "p Ong":
-            translated += "r"
-        elif pong == "p OnG":
-            translated += "R"
-        elif pong == "pO ng":
-            translated += "s"
-        elif pong == "pO nG":
-            translated += "S"
-        elif pong == "pOn g":
-            translated += "t"
-        elif pong == "pOn G":
-            translated += "T"
-        elif pong == "p O ng":
-            translated += "u"
-        elif pong == "p O nG":
-            translated += "U"
-        elif pong == "pO n g":
-            translated += "v"
-        elif pong == "pO n G":
-            translated += "V"
-        elif pong == "p On g":
-            translated += "w"
-        elif pong == "p On G":
-            translated += "W"
-        elif pong == "p O n g":
-            translated += "x"
-        elif pong == "p O n G":
-            translated += "X"
-        # y and z
-        elif pong == "poNg":
-            translated += "y"
-        elif pong == "poNG":
-            translated += "Y"
-        elif pong == "p oNg":
-            translated += "z"
-        elif pong == "p oNG":
-            translated += "Z"
-        # any others not defined
-        elif pong.lower() == "end":
-            end = True
-        else:
-            translated += pong
+# Credit to AJH for this 
+def fast_eng_to_pong(message):
+    """A fast but less accurate pong-to-english translator. Unreliable for all dialects of pong."""
+    valid_characters = ["p", "o", "n", " ", "P", "O", "N"]                # g is not in here. this will make sense later
+    pong_dict = pong_to_eng_dict()                      # dictionary for pong>eng translations
+    translation = ""
+    current_pong = ""
+    for character in message:                                      # for each character in the string
 
-    translated += " "
-    print(translated)
+        # to prevent pong separation in the message from throwing the program off
+        if current_pong == "" and character == " ":
+            pass
+
+        elif character in valid_characters:
+            current_pong += character
+
+        # each pong starts with p and ends with g, no matter what.
+        # so by testing where the g is, we can find where each character ends and the next one starts.
+        elif character == "g":
+            current_pong += character
+            translation += pong_dict[current_pong]
+            current_pong = ""
+        # in case it is uppercase
+        elif character == "G":
+            current_pong += "g"
+            translation += pong_dict[current_pong].upper()
+            current_pong = ""
+
+        elif character == ")":                                 # ) identify when the word ends.
+            translation += " "
+
+        # this is to take care of special characters like commas or exclamation points.
+        elif character != "(":               # ( are not necessary when we know ) end a word
+            translation += character
+
+        print(translation)
 
 
 def run():
